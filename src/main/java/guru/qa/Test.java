879da787d553bb236e72e28bdfe28643e7d1fbfa
Krl5to <i.petretskiy@gmail.com>
2023-08-27 14:09:19 +0300

    Add arithmetic operators

---
+++ src/main/java/guru/qa/Test.java
@@ -0,0 +1,23 @@
+package guru.qa;
+
+public class Test {
+
+    public static void main(String args[]) {
+        int a = 20;
+        int b = 40;
+        int c = 50;
+        int d = 50;
+
+        // Арифметические операторы
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++   = " +  (a++));
+        System.out.println("b--   = " +  (b--));
+        System.out.println("d++   = " +  (d++));
+        System.out.println("++d   = " +  (++d));
+    }
+}
